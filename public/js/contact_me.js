e3af244a40d25a57df0ebb9e2e18b4181c5193ce
Pechanikov Dmitry <pechatny@gmail.com>
2016-02-23 23:11:44 +0400

    email from validation errors

--- public/js/contact_me.js
+++ public/js/contact_me.js
@@ -41,7 +41,7 @@ $(function() {
                     $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                         .append("</button>");
                     $('#success > .alert-success')
-                        .append("<strong>Your message has been sent. </strong>");
+                        .append("<strong>Ваше сообщение отправлено. </strong>");
                     $('#success > .alert-success')
                         .append('</div>');
 
@@ -53,7 +53,7 @@ $(function() {
                     $('#success').html("<div class='alert alert-danger'>");
                     $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                         .append("</button>");
-                    $('#success > .alert-danger').append("<strong>Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!");
+                    $('#success > .alert-danger').append("<strong>Извините " + firstName + ", кажется мой почтовый сервер не отвечает. Пожалуйста попробуйте ещё раз!");
                     $('#success > .alert-danger').append('</div>');
                     //clear all fields
                     $('#contactForm').trigger("reset");
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -206,28 +206,28 @@
                     <div class="row control-group">
                         <div class="form-group col-xs-12 floating-label-form-group controls">
                             <label>Имя</label>
-                            <input type="text" class="form-control" placeholder="Имя" id="name" required data-validation-required-message="Please enter your name.">
+                            <input type="text" class="form-control" placeholder="Имя" id="name" required data-validation-required-message="Пожалуйста укажите имя.">
                             <p class="help-block text-danger"></p>
                         </div>
                     </div>
                     <div class="row control-group">
                         <div class="form-group col-xs-12 floating-label-form-group controls">
                             <label>Email</label>
-                            <input type="email" class="form-control" placeholder="Email" id="email" required data-validation-required-message="Please enter your email address.">
+                            <input type="email" class="form-control" placeholder="Email" id="email" required data-validation-required-message="Пожалуйста укажите ваш email адрес.">
                             <p class="help-block text-danger"></p>
                         </div>
                     </div>
                     <div class="row control-group">
                         <div class="form-group col-xs-12 floating-label-form-group controls">
                             <label>Телефон</label>
-                            <input type="tel" class="form-control" placeholder="Телефон" id="phone" required data-validation-required-message="Please enter your phone number.">
+                            <input type="tel" class="form-control" placeholder="Телефон" id="phone" required data-validation-required-message="Пожалуйста укажите ваш номер телефона.">
                             <p class="help-block text-danger"></p>
                         </div>
                     </div>
                     <div class="row control-group">
                         <div class="form-group col-xs-12 floating-label-form-group controls">
                             <label>Сообщение</label>
-                            <textarea rows="5" class="form-control" placeholder="Сообщение" id="message" required data-validation-required-message="Please enter a message."></textarea>
+                            <textarea rows="5" class="form-control" placeholder="Сообщение" id="message" required data-validation-required-message="Пожалуйтва оставьте сообщение."></textarea>
                             <p class="help-block text-danger"></p>
                         </div>
                     </div>
